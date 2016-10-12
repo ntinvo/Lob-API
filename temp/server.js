963393caa8587d8c95d019cdbae4a0d6f0e42f8a
Tin Vo <tinnvo1101@gmail.com>
2016-10-12 12:31:13 -0500

    Created url

--- temp/server.js
+++ temp/server.js
@@ -3,9 +3,15 @@ var express = require('express');
 var app     = express();
 var path    = require('path');
 var bodyParser	= require('body-parser');
+var request = require('request');
 var fs = require('fs');
+
+// Lob
 var lobFactory = require('./lib/index.js');
 var Lob = new lobFactory('test_fd34e1b5ea86a597ec89f7f2e46940c874d');
+
+// Google API
+var google_api_key = "AIzaSyBH7ovb41j8HaaYhJqolwW4Ury5dfAL_e0";
 
 // Read letter.html template
 var file = fs.readFileSync(__dirname + '/static/html/letter.html').toString();
@@ -26,6 +32,7 @@ app.post('/send', function(req, res) {
 	var repContact = getRepContact(req.body)
 });
 
+// Get rep's information
 var getRepContact = function(userInfo) {
 	var address1 = userInfo.address1 == undefined ? "" : userInfo.address1;
 	var address2 = userInfo.address2 == undefined ? "" : userInfo.address2;
@@ -33,8 +40,10 @@ var getRepContact = function(userInfo) {
 	var state = userInfo.state == undefined ? "" : userInfo.state;
 	var zipcode = userInfo.zipcode == undefined ? "" : userInfo.zipcode;	
 	var url = address1 + "+" + address2 + "+" + city + "+" + state + "+" + zipcode;
-	url = url.replace("++", "+").replace(" ", "+");
+	url = url.split("++").join("+");
+	url = url.split(" ").join("+");
 
+	url = "https://www.googleapis.com/civicinfo/v2/representatives?address=" + url + "&key=" + google_api_key
 	console.log(url);
 };
 
