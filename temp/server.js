5851bd9377f290bcd847b59a78d7b222d07ab965
Tin Vo <tinnvo1101@gmail.com>
2016-10-12 12:14:30 -0500

    Getting rep's information

--- temp/server.js
+++ temp/server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app     = express();
 var path    = require('path');
-
+var bodyParser	= require('body-parser');
 var fs = require('fs');
 var lobFactory = require('./lib/index.js');
 var Lob = new lobFactory('test_fd34e1b5ea86a597ec89f7f2e46940c874d');
@@ -13,20 +13,31 @@ var file = fs.readFileSync(__dirname + '/static/html/letter.html').toString();
 // Set static folder
 app.use(express.static(__dirname + '/static'));
 
+// Parse body
+app.use(bodyParser.json());
+
 // Root route
 app.get('/',function(req,res){
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 // Catch the send request
-app.get('/send', function(req, res) {
-	console.log(10);
-})
+app.post('/send', function(req, res) {
+	var repContact = getRepContact(req.body)
+});
 
+var getRepContact = function(userInfo) {
+	var address1 = userInfo.address1 == undefined ? "" : userInfo.address1;
+	var address2 = userInfo.address2 == undefined ? "" : userInfo.address2;
+	var city = userInfo.city == undefined ? "" : userInfo.city;
+	var state = userInfo.state == undefined ? "" : userInfo.state;
+	var zipcode = userInfo.zipcode == undefined ? "" : userInfo.zipcode;	
+	var url = address1 + "+" + address2 + "+" + city + "+" + state + "+" + zipcode;
+	url = url.replace("++", "+").replace(" ", "+");
 
-var repContact = function() {
-	
-}
+	console.log(url);
+};
 
+// Listen on port 8000
 app.listen(8000);
 console.log("Running at Port 8000");
--- temp/static/js/letterController.js
+++ temp/static/js/letterController.js
@@ -3,10 +3,9 @@ var app = angular.module('sendLetterApp', []);
 
 // main app controller
 app.controller('mainCtrl', function($scope, $http) {
-	$scope.sendLetter = function() {
-		// send GET request to the server
-		$http.get('/send').success(function(res) {
-			console.log("Done");
+	$scope.sendLetter = function(req, res) {
+		$http.post('/send', $scope.user).success(function(res) {
+			console.log(res);
 		});
 	};
 });
